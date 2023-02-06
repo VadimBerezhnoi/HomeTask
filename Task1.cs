167610d4abed4f350f824be9d142d1a348e03672
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-06 19:22:23 +0300

    Изменен код

---
+++ Task1.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+int num = ReadInt("Введите трехзначное число: ");
+int a = num.ToString().Length;
+
+if (a < 3 || a > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(num));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
